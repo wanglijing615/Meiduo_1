0363ba5c70d2123419cec33e3534f5b3b89304a9
wanglijing615 <732466233@qq.com>
2019-10-18 20:41:35 +0800

    设置jinja2模板及mysql配置信息

--- Meiduo_1/Meiduo_1/settings.py
+++ Meiduo_1/Meiduo_1/settings.py
@@ -92,14 +92,24 @@ WSGI_APPLICATION = 'Meiduo_1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+##########配置mysqldb连接信息################################################
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.mysql', # 数据库引擎
+        'HOST': '127.0.0.1', # 数据库主机
+        'PORT': 3306, # 数据库端口
+        'USER': 'root', # 数据库用户名
+        'PASSWORD': 'mysql', # 数据库用户密码
+        'NAME': 'meiduo_db1' # 数据库名字
+    },
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
