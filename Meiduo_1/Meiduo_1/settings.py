a9582a363c7c6ae21053f7ccfc63a565c7ec9f2a
wanglijing615 <732466233@qq.com>
2019-10-18 08:29:10 +0800

    配置jinjia2模板环境

--- Meiduo_1/Meiduo_1/settings.py
+++ Meiduo_1/Meiduo_1/settings.py
@@ -51,10 +51,26 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'Meiduo_1.urls'
 
+# TEMPLATES = [
+#     {
+#         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+#         'DIRS': [],
+#         'APP_DIRS': True,
+#         'OPTIONS': {
+#             'context_processors': [
+#                 'django.template.context_processors.debug',
+#                 'django.template.context_processors.request',
+#                 'django.contrib.auth.context_processors.auth',
+#                 'django.contrib.messages.context_processors.messages',
+#             ],
+#         },
+#     },
+# ]
+########################jinjia2模板######################################
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'BACKEND': 'django.template.backends.jinja2.Jinja2',  # jinja2模板引擎
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -63,9 +79,12 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            # 补充Jinja2模板引擎环境
+            'environment': 'utils.jinja2_env.jinja2_environment',
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'Meiduo_1.wsgi.application'
 
